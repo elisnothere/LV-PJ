a2eb1ab57f24e457139392bbd202938a42a16de5
nicolasmontielf <nicolasmontielf@gmail.com>
2026-07-31 16:45:44 -0300

    fix: UI on register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema - Crear cuenta</title>
+    <title>Sistema - Crear cuenta nueva</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
 </head>
